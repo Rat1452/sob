72b0e9d20dbf7f7b9b6b04fda9880cb4dc916b7d
Rat <markreigle52@gmail.com>
2025-01-12 18:39:36 -0500

    push

--- addons/weapons/cfgWeapons.hpp
+++ addons/weapons/cfgWeapons.hpp
@@ -8,5 +8,20 @@ class Mode_Burst;
 
 class CfgWeapons
 {
+	class JLTS_DC15A;
+	class JLTS_DC15A_plastic : JLTS_DC15A {
+		class WeaponSlotsInfo;
+	};
+
+	class 3AS_DC15L_Base_F;
+	class 3AS_DC15L_F: 3AS_DC15L_Base_F {
+		class WeaponSlotsInfo;
+		class FullAuto;
+	};
+
+	class 3AS_Valken38X_Base_F;
+	class 3AS_Valken38X_F: 3AS_Valken38X_Base_F {
+		class WeaponSlotsInfo;
+	};
 	#include "weapons\primaries.hpp"
 };
